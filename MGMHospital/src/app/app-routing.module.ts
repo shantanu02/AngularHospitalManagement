ceb59ba59d24b33aae1e6ae2901c3fc3d2ca2d69
AshwiniJadhav3330 <ashwinijadhav3330@gmail.com>
2021-01-17 11:13:28 +0530

    Registration For Patient

--- MGMHospital/src/app/app-routing.module.ts
+++ MGMHospital/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationPatientComponent } from './registration-patient/registration-patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'registration-patient',component: RegistrationPatientComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MGMHospital/src/app/app.module.ts
+++ MGMHospital/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationPatientComponent } from './registration-patient/registration-patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPatientComponent
   ],
   imports: [
     BrowserModule,
