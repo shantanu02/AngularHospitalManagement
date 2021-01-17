c1cf3f7483965947c9ada69e37f11729cc69853d
realamolshinde <realamolshinde@gmail.com>
2021-01-17 23:32:56 +0530

    Home Page

--- MGMHospital/src/app/app-routing.module.ts
+++ MGMHospital/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { DepartmentComponent } from './department/department.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'homepage', component:HomepageComponent},
+  {path:'about', component:AboutComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'department', component:DepartmentComponent},
+  {path:'doctor', component:DoctorComponent},
+
+  {path:'', redirectTo:'homepage', pathMatch:'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MGMHospital/src/app/app.module.ts
+++ MGMHospital/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { DepartmentComponent } from './department/department.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    DepartmentComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
