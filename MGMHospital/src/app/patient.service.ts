2d9a244c0569459fa041d415dcf5e208e1394458
shantanu02 <shantanuupase02@gmail.com>
2021-01-19 21:51:19 -0600

    patient registration

---
+++ MGMHospital/src/app/patient.service.ts
@@ -0,0 +1,178 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+export class PatientInformation{
+  constructor(
+    public patientId:number,
+    public patientFName:string,
+    public patientLName:string,
+    public patientEmail:string,
+    public patientPassword:string,
+    public patientAddress:string,
+    public patientTempAddress:string,
+    public patientCity:string,
+    public patientZipCode:string,
+    public patientState:string,
+    public patientGender:string,
+    public patientDOB:string,
+    public patientPhone:string,
+    public patientWeight:number,
+    public patientTriage:string,
+    public patientLMP:string,
+    public patientComplaint : string,
+    public patientAllergies:string,
+    public patientMedicalHistory:string,
+    public patientSID :string,
+    public patientOPD :string,
+    public patientIPD :string,
+    public patientWard :string,
+    public patientBedNo :string,
+    public doctorId :number,
+    public nurseId :number
+
+  ){}
+}
+
+export class PatientExamination{
+  constructor(
+   
+
+    public peId:number,
+    public peDateTime :string,
+    public peHead :string,
+    public peNeck :string,
+    public peChest:string,
+    public peHeart:string,
+    public peNeuro:string,
+    public peAbdomen:string,
+    public peExtermities:string,
+    public peOthers:string,
+    public pePulse:string,
+    public peResp:string,
+    public peBp:string,
+    public peTemp:string,
+    public pePulseOx:string,
+    public peFSBS:string,
+    public patientId:number
+
+
+  ){}
+}
+
+export class PatientTreatment {
+  constructor(
+
+    public ptId:number,
+    public ptNurseNote :string,
+    public ptMDNote:string,
+    public ptDoctorNote :string,
+    public peId:number,
+  
+
+  ){}
+  
+}
+export class PatientMedicines{
+  constructor(
+    public pmId:number,
+    public pmDosage:string,
+    public pmTime:string,
+    public ptId:number,
+    public medicineId:number
+  ){}
+}
+
+export class PatientTest{
+  constructor(
+    public pTestId:number,
+    public pTestConsultant:string,
+    public pTestStatus:string,
+    public medicalTestId:number,
+    public ptId:number
+  ){}
+  
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PatientService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  AddPatientInformation(patientInformation : PatientInformation){
+      return this.httpClient.post<number>("http://localhost:8080/patient/addPatientInformation",patientInformation);
+
+  }
+
+  AddPatientExamination(patientExamination : PatientExamination){
+    return this.httpClient.post<number>("http://localhost:8080/patient/addPatientExamination",patientExamination);
+  }
+ 
+
+  AddPatientTreatment(patientTreatment : PatientTreatment){
+    return this.httpClient.post<number>("http://localhost:8080/patient/addPatientTreatment",patientTreatment);
+  }
+
+  AddPatientMedicines(patientMedicines: PatientMedicines){
+    return this.httpClient.post<number>("http://localhost:8080/patient/addPatientMedicines",patientMedicines);
+  }
+
+  AddPatientTest(patientTest : PatientTest){
+    return this.httpClient.post<number>("http://localhost:8080/patient/addPatientTest",patientTest);
+  }
+
+  DeletePatientInformation(patientId : number){
+    return this.httpClient.delete<number>("http://localhost:8080/patient/deletePatientInformation/"+patientId);
+
+  }
+
+  DeletePatientExamination(peId:number){
+    return this.httpClient.delete<number>("http://localhost:8080/patient/deletePatientExamination/"+peId);
+  }
+
+  DeletePatientTreatment(ptId:number){
+    return this.httpClient.delete<number>("http://localhost:8080/patient/deletePatientTreatment/"+ptId);
+  }
+
+  DeletePatientMedicines(pmId:number){
+    return this.httpClient.delete<number>("http://localhost:8080/patient/deletePatientMedicines/"+pmId);
+  }
+
+  DeletePatientTest(pTestId:number){
+    return this.httpClient.delete<number>("http://localhost:8080/patient/deletePatientTest/"+pTestId);
+  }
+
+  UpdatePatientInformation(patientInformation:PatientInformation){
+    return this.httpClient.put<number>("http://localhost:8080/patient/updatePatientInformation",patientInformation);
+  }
+
+  UpdatePatientExamination(patientExamination:PatientExamination){
+    return this.httpClient.put<number>("http://localhost:8080/patient/updatePatientExamination",patientExamination);
+  }
+
+  UpdatePatientTreatment(patientTreatment:PatientTreatment){
+    return this.httpClient.put<number>("http://localhost:8080/patient/updatePatientTreatment",patientTreatment);
+  }
+
+  UpdatePatientMedicines(patientMedicines:PatientMedicines){
+    return this.httpClient.put<number>("http://localhost:8080/patient/updatePatientMedicines",patientMedicines);
+  }
+
+  UpdatePatientTest(patientTest:PatientTest){
+    return this.httpClient.put<number>("http://localhost:8080/patient/updatePatientTest",patientTest);
+  }
+
+  GetPatientInformation(patientId:number){
+    return this.httpClient.get<PatientInformation[]>("http://localhost:8080/patient/getPatientInformation/"+patientId);
+  }
+
+  
+  
+
+
+}
